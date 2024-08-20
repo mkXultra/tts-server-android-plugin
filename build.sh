ad61dfe4d058fabb58b1e75c090f4ec8bfad1f80
mkXultra <electrincgoat@gmail.com>
2024-08-20 20:39:59 +0900

    feat: multi build support

--- build.sh
+++ build.sh
@@ -1,1 +1,13 @@
-jq --rawfile code openai.js '.[0].code = $code' openai_base.json > openai.json
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+    echo "Error: No target specified. Usage: $0 <target>"
+    echo "target: gcp, azure, openai"
+    echo "example: $0 gcp"
+    exit 1
+fi
+
+target=$1
+target_base="${target}_base"
+
+jq --rawfile code ./src/$target/$target.js '.[0].code = $code' ./src/$target/$target_base.json > ./src/$target/$target.json
